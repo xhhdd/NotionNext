e59f26ae36e1ed4c7e2907844334538014670860
xhhdd <91271854+xhhdd@users.noreply.github.com>
2022-12-16 23:02:15 +0800

    Update Header.js

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -75,7 +75,7 @@ const Header = props => {
       className="duration-500 md:bg-fixed w-full bg-cover bg-center h-screen bg-black text-white"
       style={{
         backgroundImage:
-          `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0,0,0,0.5), rgba(0,0,0,0.3), rgba(0,0,0,0.5), rgba(0, 0, 0, 0.9) ),url("${siteInfo?.pageCover}")`
+          `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0,0,0,0.3), rgba(0,0,0,0.1), rgba(0,0,0,0.3), rgba(0, 0, 0, 0.8) ),url("${siteInfo?.pageCover}")`
       }}
     >
       <div className="absolute flex flex-col h-full items-center justify-center w-full ">
